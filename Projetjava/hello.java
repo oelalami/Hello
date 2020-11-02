0ee9c56684f8c9a3f1adfe31bd66e4876b4ee38e
el.alami.omar@gmail.com <en03845@i154708-ti.sim.umontreal.ca>
2020-11-02 08:11:11 -0500

    add v7

--- Projetjava/hello.java
+++ Projetjava/hello.java
@@ -3,12 +3,7 @@ public class hello {
     public static void main (String[] args){
 
         for (int i=1;i<=10;i++){
-			
-<<<<<<< HEAD
-	         System.out.println("Hello World...v6"+i);
-=======
-	         System.out.println("Hello World V4..."+i);
->>>>>>> ddca00d7d39b1c9f836ac53ec635b57ba80e3b5e
+		System.out.println("Hello World ..v7"+i);	
 		}
 		
 	}
